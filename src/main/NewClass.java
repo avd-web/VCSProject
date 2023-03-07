1c8207d9700899c2008251f4ac34769625c974c7
avd-web <avd-web@outlook.com>
2023-03-07 13:31:56 +0100

    test commit2

--- src/main/NewClass.java
+++ src/main/NewClass.java
@@ -2,6 +2,9 @@ package main;
 
 public class NewClass {
     public static void main(String[] args) {
+
         System.out.println("test NewClass class");
+        System.out.println("test commit");
     }
+
 }
